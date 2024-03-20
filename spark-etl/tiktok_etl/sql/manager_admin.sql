6640d33d7e214c1ca7623b7aaecff22f58dafabd
stekitay1 <steven.kitay0123@gmail.com>
2024-03-20 10:26:06 -0400

    updated aflac prod collection proc for tiktok

--- spark-etl/tiktok_etl/sql/manager_admin.sql
+++ spark-etl/tiktok_etl/sql/manager_admin.sql
@@ -1,3 +1,9 @@
 call tiktok_staging.key_standard();
 call tiktok_staging.fact_daily_standard()
-call tiktok_staging.clear_queues()
+
+select report_status, count(*) from tiktok_staging.job_manager group by 1;
+
+select staging_status, count(*) from tiktok_staging.query_data group by 1;
+
+call tiktok_staging.clear_queues();
+call tiktok_prod.aflac_standard();
---
+++ spark-etl/tiktok_etl/sql/prod _collections.sql
@@ -0,0 +1,80 @@
+create or replace procedure tiktok_prod.aflac_standard()
+language plpgsql
+as $$ begin
+
+drop table tiktok_prod.aflac_standard;
+
+create table tiktok_prod.aflac_standard as 
+select 
+	fds.account_id as account_id
+    ,'TikTok' as platform
+	,fds.campaign_id as campaign_id
+    ,cmp.name as campaign_name
+    ,cmp.objective as campaign_objective
+    ,cmp.optimization_goal as campaign_opt_goal
+	,fds.adgroup_id as adgroup_id
+    ,ag.name as adgroup_name
+    ,ag.objective as adgroup_objective
+    ,ag.optimization_goal as adgroup_opt_goal
+    ,ag.attribution_setting as adgroup_att_setting
+	,fds.ad_id as ad_id
+    ,ad.name as ad_name
+	,fds.date as date
+	,ad.dcm_clicktag as clicktag
+	,coalesce(fds.impressions, 0) as impressions
+	,coalesce(fds.link_clicks, 0) as clicks
+	,coalesce(fds.spend, 0) as spend
+	,coalesce(fds.video_starts, 0) as video_starts
+	,coalesce(fds.video_completes, 0) as video_completes
+    ,coalesce(fds.ad_likes, 0) as ad_like
+	,coalesce(fds.ad_shares, 0) as ad_share
+	,coalesce(fds.ad_comments, 0) as ad_comment
+	,0 as ad_save
+	,coalesce(fds.page_likes, 0) as page_like
+
+from (
+        select 
+            account_id
+            ,campaign_id
+            ,adgroup_id
+            ,ad_id
+            ,date
+            ,impressions
+            ,link_clicks
+            ,spend
+            ,video_starts
+            ,video_completes
+			,ad_likes
+			,ad_shares
+			,ad_comments
+			,page_likes
+            from tiktok.fact_daily_standard
+        where account_id in (7174850463932678145,7270655699670073345)) as fds
+left join (
+        select 
+            name
+            ,id
+            ,account_id
+            ,objective
+            ,optimization_goal
+        from tiktok.campaigns
+        where account_id in (7174850463932678145,7270655699670073345)) as cmp on cmp.id = fds.campaign_id
+left join (
+        select 
+            name
+            ,id
+            ,account_id
+            ,objective
+            ,optimization_goal
+            ,attribution_setting
+        from tiktok.adgroups
+		where account_id in (7174850463932678145,7270655699670073345)) as ag on (ag.account_id, ag.id) = (fds.account_id, fds.adgroup_id)
+left join (
+        select
+            name
+            ,id
+            ,account_id
+            ,dcm_clicktag
+        from tiktok.ads
+        where account_id in (7174850463932678145,7270655699670073345)) as ad on (ad.account_id, ad.id) = (fds.account_id, fds.ad_id);
+end;$$;
