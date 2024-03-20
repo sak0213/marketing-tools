b889059cdff9b304290b5c04367f0d8167e6a77e
stekitay1 <steven.kitay0123@gmail.com>
2024-03-20 10:25:42 -0400

    updated aflac prod collection proc

--- spark-etl/facebook_etl/sql/manager_admin.sql
+++ spark-etl/facebook_etl/sql/manager_admin.sql
@@ -2,7 +2,9 @@ call facebook_staging.key_standard();
 call facebook_staging.fact_daily_standard();
 call facebook_staging.fact_conversion();
 
+select report_status, count(*) from facebook_staging.job_manager group by 1;
 
+select staging_status, count(*) from facebook_staging.query_data group by 1;
 
 update facebook.accounts
 set
@@ -12,12 +14,8 @@ from (select cast(account_id as bigint) as account_id from facebook_staging.quer
 where facebook.accounts.id = a.account_id;
 
 
-select report_status, count(*) from facebook_staging.job_manager group by 1;
-
-select staging_status, count(*) from facebook_staging.query_data group by 1;
-
 call facebook_staging.clear_queues();
 call facebook_prod.napa_standard();
 call facebook_prod.jameshardie_standard();
-
+call facebook_prod.aflac_standard();
 
