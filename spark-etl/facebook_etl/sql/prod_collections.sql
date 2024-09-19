89927adbe9c07757b1c9244dc4ab61826320e10d
stekitay1 <steven.kitay0123@gmail.com>
2024-09-19 14:03:19 -0400

    Added new prod collection

--- spark-etl/facebook_etl/sql/prod_collections.sql
+++ spark-etl/facebook_etl/sql/prod_collections.sql
@@ -408,3 +408,95 @@ left join (
 
 
 end;$$;
+
+
+-- humana pharmacy proc
+create or replace procedure facebook_prod.xumo_daily_standard()
+language plpgsql
+as $$ begin
+drop table facebook_prod.xumo_daily_standard;
+
+create table facebook_prod.xumo_daily_standard as
+
+select 
+	fds.account_id as account_id
+    ,'Meta' as platform
+	,fds.campaign_id as campaign_id
+    ,cmp.name as campaign_name
+    ,cmp.objective as campaign_objective
+    ,cmp.optimization_goal as campaign_opt_goal
+	,fds.adgroup_id as adgroup_id
+    ,ag.name as adgroup_name
+    ,ag.objective as adgroup_objective
+    ,ag.optimization_goal as adgroup_opt_goal
+    ,ag.attribution_setting as adgroup_att_setting
+	,fds.ad_id as ad_id
+    ,ad.name as ad_name
+	,fds.date as date
+	,coalesce(fds.impressions, 0) as impressions
+	,coalesce(fds.link_clicks, 0) as clicks
+	,coalesce(fds.spend, 0) as spend
+	,coalesce(fds.video_starts, 0) as video_starts
+	,coalesce(fds.video_completes, 0) as video_completes
+    ,coalesce(cast(fda.count_value ->> 'post_reaction' as int), 0) as ad_like
+    ,coalesce(cast(fda.count_value ->> 'post' as int), 0) as ad_share
+    ,coalesce(cast(fda.count_value ->> 'comment' as int), 0) as ad_comment
+    ,coalesce(cast(fda.count_value ->> 'like' as int), 0) as page_like
+    ,coalesce(cast(fda.count_value ->> 'onsite_conversion.post_save' as int), 0) as ad_save
+from (
+        select 
+            fds.account_id
+            ,fds.campaign_id
+            ,fds.adgroup_id
+            ,fds.ad_id
+            ,fds.date
+            ,fds.impressions
+            ,fds.link_clicks
+            ,fds.spend
+            ,fds.video_starts
+            ,fds.video_completes
+            from facebook.fact_daily_standard as fds
+        where account_id = 577292516735129) as fds
+left join (
+        select 
+            ad_id
+            ,adgroup_id
+            ,campaign_id
+            ,account_id
+            ,date
+            ,json_object_agg(action_name, count_7dc) as count_7dc
+            ,json_object_agg(action_name, count_1dv) as count_1dv
+        ,json_object_agg(action_name, count_value) as count_value
+        from facebook.fact_daily_actions
+        where account_id = 577292516735129
+        group by 1,2,3,4,5
+        order by date) as fda on (fda.account_id, fda.ad_id, fda.date) = (fds.account_id, fds.ad_id, fds.date)
+left join (
+        select 
+            name
+            ,id
+            ,account_id
+            ,objective
+            ,optimization_goal
+        from facebook.campaigns
+        where account_id = 577292516735129) as cmp on (cmp.account_id, cmp.id) = (fds.account_id, fds.campaign_id)
+left join (
+        select 
+            name
+            ,id
+            ,account_id
+            ,objective
+            ,optimization_goal
+            ,attribution_setting
+        from facebook.adgroups
+        where account_id = 577292516735129) as ag on (ag.account_id, ag.id) = (fds.account_id, fds.adgroup_id)
+left join (
+        select
+            name
+            ,id
+            ,account_id
+        from facebook.ads
+        where account_id = 577292516735129) as ad on (ad.account_id, ad.id) = (fds.account_id, fds.ad_id);
+
+
+end;$$;
